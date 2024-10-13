1ca57780109b450764f6433011b26f87b1e25ad2
thielmeiers <thielmeiers25@hanover.edu>
2024-10-13 10:22:55 -0400

    started to switch animations from using the AnimatedSprite2D to start using the AnimationPlayer and AnimationTree. Player script working now

--- docs/game/mygame/player.gd
+++ docs/game/mygame/player.gd
@@ -1,96 +1,64 @@
-
 extends CharacterBody2D
-# Sets the move speed of the player (change to increase/decrease)
+
 var SPEED = 100.0
-# Declares the last direction as a variable to call
 var last_direction = Vector2.ZERO
-var animated_sprite
 var enemy_in_range = false
 var is_attacking = false
 
-# Is like the setup function in Arduino
-func _ready():
-	#platform_floor_layers = false
-	# refrences the AnimatedSprite2D player node as the animated_sprite variable
-	animated_sprite = $AnimatedSprite2D
-	add_to_group("Player")
-	
-	# makes the player face down in direction at start of the scene playing and plays the idle_down animation right away
-	last_direction = Vector2(0,1)
-	animated_sprite.play("idle_down")
+var direction : Vector2 = Vector2.ZERO
 
+@onready var animation_tree : AnimationTree = $AnimationTree
+
+func _ready():
+	animation_tree.active = true
+	add_to_group("Player")
+
+func _process(delta):
+	update_animation_parameters()
+
+@warning_ignore("unused_parameter")
 func _physics_process(delta):
-	# X is -1 if 'left' is pressed, 1 if 'right' is pressed, and 0 otherwise
-	# Y is -1 if 'up' is pressed, 1 if 'down' is pressed, and 0 otherwise
-	#
-	# Mapped 'left' to 'A', 'right' to 'D', 'up' to 'W', and 'down' to 'S' in Project -> Project Settings -> Input Map
-	var direction = Input.get_vector("left","right","up","down")
-	# checks if player is currently attacking and stops movement if player is
+	# Prevent movement if player is attacking
 	if is_attacking:
-		velocity = direction
-	if not is_attacking:
+		velocity = Vector2.ZERO
+	else:
+		# Get input direction and move the character if not attacking
+		direction = Input.get_vector("left", "right", "up", "down").normalized()
 		velocity = direction * SPEED
-	
-	# Remembers last direction player was facing
-	if direction != Vector2.ZERO:
+
+	if direction != Vector2.ZERO and not is_attacking:
 		last_direction = direction
 
-	# Handle attack input first
-	if Input.is_action_pressed("swing"):
-		# when player is attacking will stop player movement
-		is_attacking = true
-		# Play the attack animation based on last direction
-		if last_direction.x < 0:
-			animated_sprite.play("swing_left")
-		elif last_direction.x > 0:
-			animated_sprite.play("swing_right")
-		elif last_direction.y < 0:
-			animated_sprite.play("swing_up")
-		elif last_direction.y > 0:
-			animated_sprite.play("swing_down")
-			
-	elif Input.is_action_pressed("smash"):
-		# when player is attacking will stop player movement
-		is_attacking = true
-		# Play the attack animation based on last direction
-		if last_direction.x < 0:
-			animated_sprite.play("smash_left")
-		elif last_direction.x > 0:
-			animated_sprite.play("smash_right")
-		elif last_direction.y < 0:
-			animated_sprite.play("smash_up")
-		elif last_direction.y > 0:
-			animated_sprite.play("smash_down")
-	else:
-		# will resume player movement when not attacking
-		is_attacking = false
-		# Handle movement input
-		if direction.x < 0:
-			animated_sprite.play("walk_left")
-		elif direction.x > 0:
-			animated_sprite.play("walk_right")
-		elif direction.y < 0:
-			animated_sprite.play("walk_up")
-		elif direction.y > 0:
-			animated_sprite.play("walk_down")
-		else:
-			# Handle idle based on last direction
-			if last_direction.x < 0:
-				animated_sprite.play("idle_left")
-			elif last_direction.x > 0:
-				animated_sprite.play("idle_right")
-			elif last_direction.y < 0:
-				animated_sprite.play("idle_up")
-			elif last_direction.y > 0:
-				animated_sprite.play("idle_down")
-			
 	move_and_slide()
+
+func update_animation_parameters():
+	# Handle idle/walking animations
+	if velocity == Vector2.ZERO and not is_attacking:
+		animation_tree["parameters/conditions/idle"] = true
+		animation_tree["parameters/conditions/is_walking"] = false
+	else:
+		animation_tree["parameters/conditions/idle"] = false
+		animation_tree["parameters/conditions/is_walking"] = true
+	
+	# Handle the attack animation (swing)
+	if Input.is_action_just_pressed("swing") and not is_attacking:
+		is_attacking = true
+		animation_tree["parameters/conditions/swing"] = true
+		await get_tree().create_timer(1.0).timeout  # Wait for 1 second (swing duration)
+		is_attacking = false
+		animation_tree["parameters/conditions/swing"] = false
+
+	# Update blend position (for direction in animations)
+	if direction != Vector2.ZERO:
+		animation_tree["parameters/idle/blend_position"] = direction
+		animation_tree["parameters/swing/blend_position"] = direction
+		animation_tree["parameters/walk/blend_position"] = direction
 
 func _on_hitbox_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Enemy"):
 		enemy_in_range = true
 		print("Getting attacked!")
-	
+
 func _on_hitbox_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Enemy"):
 		enemy_in_range = false
