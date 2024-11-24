db6698e5e608afeeae1e98db6f183e5ea6f45d22
thielmeiers <thielmeiers25@hanover.edu>
2024-11-24 07:47:51 -0500

    health changes

--- game/mygame/gd_scripts/healthbar.gd
+++
@@ -1,9 +0,0 @@
-extends ProgressBar
-
-func update_health(health):
-	var healthbar = $healthbar
-	healthbar.value = health
-	if health >= 100:
-		healthbar.visible = false
-	else:
-		healthbar.visible = true
